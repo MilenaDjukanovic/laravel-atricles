b26ccec1c43c290b0a6a7130f22984d8d3df8cbb
Milena <milenadjj1998@gmail.com>
2021-04-12 23:47:25 +0200

    Initial project files

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\Article;
+use App\Http\Resources\Article as ArticleResource;
+use Illuminate\Support\Facades\Log;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        Log::info(print_r($id, true));
+        $articles = Article::select('*')->where('user_id', $id)->get();
+        Log::info(print_r($articles, true));
+        
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
+
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title'); 
+        $article->body = $request->input('body');
+        $article->user_id = $request->input('user_id');
+
+        if($article -> save()) {
+            return new ArticleResource($article);
+        }
+
+        return null;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //Get a single article
+        $article = Article::findOrFail($id);
+        //Return a single article as a resource
+        return new ArticleResource($article);
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Get a single article
+        $article = Article::findOrFail($id);
+
+        if($article->delete()){
+            return new ArticleResource($article);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/* Setup CORS */
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Get articles for a certain user
+Route::get('/articles/{idUser}', 'App\Http\Controllers\ArticleController@index');
+// Route::get('articles', 'app\Http\Controllers\ArticleController@index');
+
+// List single article
+Route::get('/article/{id}', 'App\Http\Controllers\ArticleController@show');
+
+// Create new article
+Route::post('/article', 'App\Http\Controllers\ArticleController@store'); 
+
+//Update article
+Route::put('/article', 'App\Http\Controllers\ArticleController@store');
+
+//Delete article
+Route::delete('/article/{id}', 'App\Http\Controllers\ArticleController@destroy');
+
+//Login user
+Route::post('login', function (Request $request){
+    if(auth()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')])){
+        //Authentication passed...
+        $user = auth()->user();
+        $user->api_token = Str::random(60);
+        $user->save();
+        return $user;
+    }
+
+    return response()->json([
+        'error' => 'Unauthentucates user',
+        'code' => 401,
+    ], 401);
+});
+
+//Logout user
+Route::middleware('auth:api')->post('logout', function (Request $request){
+    if(auth()->user()) {
+        $user = auth()->user();
+        $user->api_token = null; //clear api token
+        $user->save();
+
+        return response()->json([
+            'message'=>'Thank you for using our application',
+        ]);
+    }
+
+    return response()->json([
+        'error' => 'Unable to logout user',
+        'code' => 401,
+    ], 401);
+});
